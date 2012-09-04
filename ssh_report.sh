6cf581d963c8447be915dff5c357fdde9c9ceb4e
Muhammad Imran Ali <nix@nix.ebricks-inc.com>
2012-09-04 14:09:26 +0500

    updated ssh report

--- ssh_report.sh
+++ ssh_report.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-Accepted=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' /var/log/auth.log | grep -i "Accepted"`
-Failure=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' /var/log/auth.log |grep -i "Failure"`
+to=`date +"%b %d %T"`
+let from_in_seconds=`date +%s`-43200
+from=`date -d @$from_in_seconds +"%b %d %T"`
+>file1
+>logs
+Logins=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' file >>file1`
 
-echo "=====================" > logs
+echo "=====================" >> logs
 echo "= Successful logins =" >> logs
 echo "=====================" >> logs
 gawk 'BEGIN {
 
-       print " Date          User          IP ";
+print " Date 		User 		IP ";
 
-       print "------       ------        ----" }
+print "------ 		------ 		----" }'
 
-cat "$Accepted" | awk {'print $1, $2, $3, $9, $11'}` >> logs
+cat file1 |grep -i Accepted | awk {'print $1, $2, $3, $9, $11'} >> logs
 echo "--------------------------------" >> logs
 echo "================================" >> logs
 
@@ -21,14 +25,14 @@ echo "= Login failures =" >> logs
 echo "==================" >> logs
 gawk 'BEGIN {
 
-       print " Date          User          IP ";
+       print " Date User IP ";
 
-       print "------       ------        ----" }
+ 
+      print "------ ------ ----" }'
 
-cat "$Failure" | awk {'print $1, $2, $3, $15, $14'}` >> logs
+cat file1 |grep -i Failure | awk {'print $1, $2, $3, $15, $14'} >> logs
 echo "--------------------------------" >> logs
 echo "================================" >> logs
 
 /usr/bin/Mailx -s "Logs for $hostname" admin@oursite.com < logs
 rm -f logs
-
