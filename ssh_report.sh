236a71a537f521475de4b2cf2a70febfc476cc1e
Muhammad Imran Ali <nix@nix.ebricks-inc.com>
2012-09-04 15:31:03 +0500

    modified:   ssh_report.sh

--- ssh_report.sh
+++ ssh_report.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+auth_log=file
+temp_log=file1
 to=`date +"%b %d %T"`
 let from_in_seconds=`date +%s`-43200
 from=`date -d @$from_in_seconds +"%b %d %T"`
 >file1
 >logs
-Logins=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' /var/log/auth.log >>file1`
+read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' $auth_log >>file1
 
 echo "=====================" >> logs
-echo "= Logins Attempts   =" >> logs
+echo "= ssh login summary =" >> logs
+echo -e "= From $from=" >> logs
+echo -e "=To $to=" >> logs
+echo -e "=====================\n\n\n" >> logs
+
+
 echo "=====================" >> logs
-total_atmp=`cat file|wc -l`
-succ_atmp=`cat file|grep -i Accepted|wc -l`
-fail_atmp=`cat file|grep -i Failure|wc -l`
-echo -e "Total Number of Attempts=" $total_atmp "\tSuccessful Attempts=" $succ_atmp "\tFailure Attempts=" $fail_atmp"\n"
+echo "= Login Attempts   =" >> logs
 echo "=====================" >> logs
-echo "= Successful logins =" >> logs
+echo -e "Total Number of Attempts=" `cat $temp_log|wc -l` "\tSuccessful Attempts=" `cat $temp_log|grep -i Accepted|wc -l` "\tFailure Attempts=" `cat $temp_log|grep -i Failure|wc -l`"\n"
+#######################
+echo "=====================" >> logs
+echo "= Login Successful =" >> logs
 echo "=====================" >> logs
 gawk 'BEGIN {
 
@@ -22,7 +29,7 @@ print " Date 		User 		IP ";
 
 print "------ 		------ 		----" }'
 
-cat file1 |grep -i Accepted | awk {'print $1, $2, $3, $9, $11'} >> logs
+cat $temp_log |grep -i Accepted | awk {'print $1, $2, $3, $9, $11'} >> logs
 echo "--------------------------------" >> logs
 echo "================================" >> logs
 
@@ -37,9 +44,10 @@ gawk 'BEGIN {
  
       print "------ ------ ----" }'
 
-cat file1 |grep -i Failure | awk {'print $1, $2, $3, $15, $14'} >> logs
+cat $temp_log |grep -i Failure | awk {'print $1, $2, $3, $15, $14'} >> logs
 echo "--------------------------------" >> logs
 echo "================================" >> logs
+
 
 /usr/bin/Mailx -s "Logs for $hostname" admin@oursite.com < logs
 rm -f logs
