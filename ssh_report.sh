73b65b5ad797553923b72ec6df6bf945467f189e
Muhammad Imran Ali <nix@nix.ebricks-inc.com>
2012-09-04 12:06:59 +0500

    ssh_report.sh added

---
+++ ssh_report.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+Accepted=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' /var/log/auth.log | grep -i "Accepted"`
+Failure=`read month day time < <(date "+%b %_d %T" -d "-12 hours"); awk -v m="$month" -v d="$day" -v t="$time" '$1 == m && $2 >= d && $3 >= t {p=1} p' /var/log/auth.log |grep -i "Failure"`
+
+echo "=====================" > logs
+echo "= Successful logins =" >> logs
+echo "=====================" >> logs
+cat "$Accepted" | awk {'print $1, $2, $3, $9, $11'}` >> logs
+echo "--------------------------------" >> logs
+echo "================================" >> logs
+
+
+echo "==================" >> logs
+echo "= Login failures =" >> logs
+echo "==================" >> logs
+cat "$Failure" | awk {'print $1, $2, $3, $15, $14'}` >> logs
+echo "--------------------------------" >> logs
+echo "================================" >> logs
+
+/usr/bin/Mailx -s "Logs for $hostname" admin@oursite.com < logs
+rm -f logs
